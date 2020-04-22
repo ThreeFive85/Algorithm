9c28c75e85239181c1330e07de28052557a8275f
ThreeFive85 <kaje0god@naver.com>
2020-04-23 01:01:26 +0900

    Fix : 알고리즘 문제 수정

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -1,33 +1,10 @@
-/**
- *
- * Implement a `DFSelect` method on this Tree class.
- *
- * DFSelect accepts a filter function, calls that function on each of the nodes
- * in Depth First order, and returns a flat array of node values of the tree
- * for which the filter returns true.
+/* Write a function that finds the largest possible product of any three numbers
+ * from an array.
  *
  * Example:
- *   var root1 = new Tree(1);
- *   var branch2 = root1.addChild(2);
- *   var branch3 = root1.addChild(3);
- *   var leaf4 = branch2.addChild(4);
- *   var leaf5 = branch2.addChild(5);
- *   var leaf6 = branch3.addChild(6);
- *   var leaf7 = branch3.addChild(7);
- *   root1.DFSelect(function (value, depth) {
- *     return value % 2;
- *   })
- *   // [1, 5, 3, 7]
+ * largestProductOfThree([2, 1, 3, 7]) === 42
  *
- *   root1.DFSelect(function (value, depth) {
- *     return depth === 1;
- *   })
- *   // [2, 3]
- *
- */
-
-/*
- * Basic tree that stores a value.
+ * Extra credit: Make your function handle negative numbers.
  */
 
 const largestProductOfThree = (array) => {
