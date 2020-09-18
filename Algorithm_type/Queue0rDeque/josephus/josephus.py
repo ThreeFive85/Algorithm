305fb898843d5c0915548434adead499da17a801
박동혁 <kaje0god@naver.com>
2020-09-18 21:29:11 +0900

    queue algorithm

---
+++ Algorithm_type/Queue0rDeque/josephus/josephus.py
@@ -0,0 +1,17 @@
+
+from sys import stdin
+
+n, k = map(int, stdin.readline().split())
+cal_list = []
+for i in range(n):
+    cal_list.append(i+1)
+
+result = []
+pop_num = 0
+while len(cal_list) > 0:
+    pop_num = (pop_num + (k - 1)) % len(cal_list)
+    pop_element = cal_list.pop(pop_num)
+    result.append(str(pop_element))
+
+result = ", ".join(result)
+print(f"<{result}>")
