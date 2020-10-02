e4020114cfbf6048037baf869eb5a37c5398afb3
박동혁 <kaje0god@naver.com>
2020-10-02 15:14:36 +0900

    dfs,bfs algorithm

---
+++ Algorithm_type/DFS_BFS/dfsAndbfs/dfsAndbfs.py
@@ -0,0 +1,55 @@
+# 문제 출처 : https://www.acmicpc.net/problem/1260
+# 참고 블로그 : https://cyc1am3n.github.io/2019/04/26/bfs_dfs_with_python.html
+
+from collections import deque
+from sys import stdin
+
+
+def DFS(graph, root):
+    visited = []
+    stack = [root]
+
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            if n in graph:
+                temp = list(set(graph[n]) - set(visited))
+                temp.sort(reverse=True)
+                stack += temp
+    return " ".join(str(i) for i in visited)
+
+
+def BFS(graph, root):
+    visited = []
+    queue = deque([root])
+
+    while queue:
+        n = queue.popleft()
+        if n not in visited:
+            visited.append(n)
+            if n in graph:
+                temp = list(set(graph[n]) - set(visited))
+                temp.sort()
+                queue += temp
+    return " ".join(str(i) for i in visited)
+
+
+graph = {}
+n = stdin.readline().split()
+node, edge, start = [int(i) for i in n]
+for i in range(edge):
+    edge_info = stdin.readline().split()
+    n1, n2 = [int(j) for j in edge_info]
+    if n1 not in graph:
+        graph[n1] = [n2]
+    elif n2 not in graph[n1]:
+        graph[n1].append(n2)
+
+    if n2 not in graph:
+        graph[n2] = [n1]
+    elif n1 not in graph[n2]:
+        graph[n2].append(n1)
+
+print(DFS(graph, start))
+print(BFS(graph, start))
